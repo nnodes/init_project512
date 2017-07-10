d8d29f91052f7285a9fd99ea18df9ac4c0b76997
Franco Restaino <francorest@gmail.com>
2017-07-10 10:59:52 -0400

    devise overrides

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,4 +6,8 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     redirect_to unauthenticated_root_path, alert: t('devise.not_available')
   end
+
+  def destroy
+    redirect_to unauthenticated_root_path, alert: t('devise.not_available')
+  end
 end
