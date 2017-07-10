9d704aa07883edfccf82da4ee1021fd0dd81060b
Juan Ignacio Cortes <jicortes2@uc.cl>
2017-07-10 15:53:18 -0400

    add images to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,9 @@
 class Page < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_attached_file :image,
+                    styles: { original: {} },
+                    path: ":rails_root/public/images/:style/:id/:filename",
+                    url: "/images/:style/:id/:filename"
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710183926) do
+ActiveRecord::Schema.define(version: 20170710194543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(version: 20170710183926) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pages_on_user_id"
   end
 
