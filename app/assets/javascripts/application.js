72cd7b15f3daa882f506224e70c7df1dcda662eb
Josefina <jose.balmacedal@gmail.com>
2017-07-12 17:23:14 -0400

    validator bootstrap style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -65,8 +65,8 @@ $(document).on('turbolinks:load', function() {
         this.findByName(element.name).addClass(errorClass).removeClass(validClass);
       } else {
         $(element).closest('.form-group').removeClass('has-success has-feedback').addClass('has-error has-feedback');
-        $(element).closest('.form-group').find('i.fa').remove();
-        $(element).closest('.form-group').append('<i class="fa fa-exclamation fa-lg form-control-feedback"></i>');
+        $(element).closest('.form-group').find('span.glyphicon').remove();
+        $(element).closest('.form-group').append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
       }
     },
     unhighlight: function (element, errorClass, validClass) {
@@ -74,8 +74,8 @@ $(document).on('turbolinks:load', function() {
         this.findByName(element.name).removeClass(errorClass).addClass(validClass);
       } else {
         $(element).closest('.form-group').removeClass('has-error has-feedback').addClass('has-success has-feedback');
-        $(element).closest('.form-group').find('i.fa').remove();
-        $(element).closest('.form-group').append('<i class="fa fa-check fa-lg form-control-feedback"></i>');
+        $(element).closest('.form-group').find('span.glyphicon').remove();
+        $(element).closest('.form-group').append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
       }
     }
   });
