58b335abbc40a1315931ad20aa6f0c790edcd469
Josefina <jose.balmacedal@gmail.com>
2017-10-19 16:25:54 -0300

    config simple form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -7,52 +7,59 @@ SimpleForm.setup do |config|
   # whole input.
   config.wrappers :default, class: :input,
     hint_class: :field_with_hint, error_class: :field_with_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
-
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
     b.use :html5
-
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input placeholder: "Placeholder"
     b.use :placeholder
-
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => true`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
-    # and/or database column lengths
     b.optional :maxlength
-
-    # Calculate minlength from length validations for string inputs
     b.optional :minlength
-
-    # Calculates pattern from format validations for string inputs
     b.optional :pattern
-
-    # Calculates min and max from length validations for numeric inputs
     b.optional :min_max
-
-    # Calculates readonly automatically from readonly attributes
     b.optional :readonly
 
     ## Inputs
-    b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+    b.wrapper :tag => 'div', :class => 'form-group' do |ba|
+      ba.use :label
+      ba.use :input, :class => 'form-control'
+      ba.use :error, :wrap_with => { :tag => 'span', :class => 'error' }
+      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'hint' }
+    end
+  end
+  config.wrappers :input_bs,
+    :tag => 'div', :class => 'form-group', 
+    :error_class => 'error', 
+    hint_class: :field_with_hint do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
 
-    ## full_messages_for
-    # If you want to display the full error message for the attribute, you can
-    # use the component :full_error, like:
-    #
-    # b.use :full_error, wrap_with: { tag: :span, class: :error }
+    ## Inputs
+    b.use :label
+    b.use :input, :class => 'form-control'
+    b.use :error, :wrap_with => { :tag => 'span', :class => 'error' }
+    b.use :hint,  :wrap_with => { :tag => 'p', :class => 'hint' }
+  end
+  config.wrappers :input_radio_inline,
+    :tag => 'div', :class => 'form-group', 
+    :error_class => 'error', 
+    hint_class: :field_with_hint do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+
+    ## Inputs
+    b.use :label
+    b.wrapper :tag => 'div', :class => 'radio-fix-inline' do |ba|
+      ba.use :input, :class => 'radio-fix'
+    end
+    b.use :error, :wrap_with => { :tag => 'span', :class => 'error' }
+    b.use :hint,  :wrap_with => { :tag => 'p', :class => 'hint' }
   end
 
   # The default wrapper to be used by the FormBuilder.
